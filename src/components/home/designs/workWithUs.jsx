fc926a3b45eaba7ed8078fb420c6defccb020bf9
Sreenath <sreenathsrp256@gmail.com>
2025-01-29 19:03:46 +0530

    ready to work with section center aligned

--- src/components/home/designs/workWithUs.jsx
+++ src/components/home/designs/workWithUs.jsx
@@ -7,7 +7,7 @@ const workWithUs = () => {
     <section className='relative flex justify-center'>
       <section className='bg-white w-full xl:w-full mx-auto text-center py-10 xl:py-20 flex flex-col gap-5'>
         <h4 className='text-5xl xl:text-7xl font-black text-black'>READY TO WORK<br /> WITH US ?</h4>
-        <p className='xl:w-10/12 w-full'>Go beyond typical with Do Studio, the <b>Best advertising agency in Calicut</b>. You’re not just choosing a campaign, you’re selecting a partner who understands your brand and has an interest in crafting genuine, meaningful and connecting stories.</p>
+        <p className='xl:w-10/12 w-full mx-auto'>Go beyond typical with Do Studio, the <b>Best advertising agency in Calicut</b>. You’re not just choosing a campaign, you’re selecting a partner who understands your brand and has an interest in crafting genuine, meaningful and connecting stories.</p>
         <ContactForm />
 
       </section>
