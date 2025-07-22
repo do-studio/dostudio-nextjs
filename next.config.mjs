6d98b4c4a05c27a8da47d69aa1b5ddab484fb731
rashid-090 <rashidpk090@gmail.com>
2025-07-22 20:42:23 +0530

    works page full dynamic

--- next.config.mjs
+++ next.config.mjs
@@ -102,6 +102,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
       {
