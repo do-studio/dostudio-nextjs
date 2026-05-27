50c659271acbb55cf503e97c9013be284f1d0aff
Sreenath <sreenathsrp256@gmail.com>
2026-05-27 12:35:11 +0530

    Meta pixel added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -72,6 +72,22 @@ export default function RootLayout({ children }) {
         `}
       </Script>
 
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1141424740995604');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
       {/* Microsoft Clarity – lazy load after user interaction */}
       <Script id="defer-analytics" strategy="afterInteractive">
         {`
@@ -99,6 +115,17 @@ export default function RootLayout({ children }) {
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1141424740995604&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+          />
         </noscript>
 
         <div className="2xl:max-w-[3500px] mx-auto">
